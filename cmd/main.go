679639461d91912fe41fcf400ed1e96c1a066c98
Peter Freese <freese.peter@gmail.com>
2018-12-02 13:01:01 -0800

    file parsing

--- cmd/main.go
+++ cmd/main.go
@@ -10,47 +10,48 @@ import (
 	"github.com/pfreese/gtex/pkg/ioutils"
 )
 
-func parseJSON() {
-	// Parse the json file
-	jsonF := "../configs/gtex.json"
-	config, err := ioutils.ParseJSON(jsonF)
-	if err != nil {
-		fmt.Println(fmt.Sprintf("error parsing JSON %s", jsonF))
-		fmt.Print(err.Error())
-	}
-
-	log.Println(config)
-}
+// The .json file constaining settings
+const jsonF string = "../configs/gtex.json"
 
 func main() {
 
+	// Make a 'logs' directory in which to write log files
 	pwd, err := os.Getwd()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println(pwd)
 	logDir := filepath.Join(pwd, "logs")
-	fmt.Println(logDir)
 	ioutils.CreateDirIfNotExist(logDir)
 
-	// Mark the timestamp in the log name: month_day_year_hour.minute.second
+	// Set up the log file
+	// Mark the timestamp in the log file name: month_day_year_hour.minute.second
 	const layout = "01_02_2006_03.04.05"
 
 	tm := time.Now().Format(layout)
-	fmt.Println(tm)
 	logBasename := fmt.Sprintf("log.%s.txt", tm)
-	file, err := os.OpenFile(filepath.Join(logDir, logBasename), os.O_RDWR | os.O_CREATE, 0666)
+	logFile, err := os.OpenFile(filepath.Join(logDir, logBasename),
+		os.O_RDWR | os.O_CREATE, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
+	defer logFile.Close()
 
-	log.SetOutput(file)
+	log.SetOutput(logFile)
 	log.Print("Logging to a file in Go!")
-	//test case
+	// test case
 	log.Println("check to make sure it works")
 
-	parseJSON()
+	// Parse the .json file
+	configuration, nil := ioutils.ParseJSON(jsonF)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Ensure all required data & metadata files exist locally
+	ioutils.EnsureFilesDownloaded(configuration)
+
+	// Finished successfully!
+	fmt.Sprintf("FINISHED! See: %s", logFile)
 }
 
--- pkg/ioutils/check_files.go
+++ pkg/ioutils/check_files.go
@@ -2,21 +2,35 @@ package ioutils
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 )
 
-// Data and annotation files were downloaded from:
-//
+// Data and annotation files were downloaded from V7 of:
+// https://gtexportal.org/home/datasets
 const (
+	annotations string = "GTEx_v7_Annotations_SampleAttributesDS.txt"
+	geneMedianTPM string = "GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_median_tpm.gct.gz"
+	geneReads string = "GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_reads.gct.gz"
 	sampleAnnot string = "GTEx_v7_Annotations_SampleAttributesDS.txt"
 )
+
+// DataFiles contains absolute paths of files
+type DataFiles struct {
+	Annotations	string
+	GeneMedianTPM	string
+	GeneReads	string
+	SampleAnnot	string
+}
 
 // Configuration tracks json config attributes
 type Configuration struct {
 	DataDIR    string
 }
 
-
+// ParseJSON parses a settings .json file and saves fields in a Configuration struct
 func ParseJSON(jsonF string) (Configuration, error) {
 	file, _ := os.Open(jsonF)
 	defer file.Close()
@@ -29,6 +43,49 @@ func ParseJSON(jsonF string) (Configuration, error) {
 	return configuration, nil
 }
 
+func EnsureFilesDownloaded(config Configuration) DataFiles {
+	dataFiles := DataFiles{}
+
+	// Check for annotations
+	annotationsF := filepath.Join(config.DataDIR, annotations)
+	if _, err := os.Stat(annotationsF); os.IsNotExist(err) {
+		panic(fmt.Sprintf("Error does not exist:", annotationsF))
+	} else {
+		log.Printf("sampleAnnot file exists: %s", annotationsF)
+		dataFiles.Annotations = annotationsF
+	}
+
+	// Check for geneMedianTPM
+	geneMedianTPMF := filepath.Join(config.DataDIR, geneMedianTPM)
+	if _, err := os.Stat(annotationsF); os.IsNotExist(err) {
+		panic(fmt.Sprintf("Error does not exist:", geneMedianTPMF))
+	} else {
+		log.Printf("GeneMedianTPM file exists: %s", geneMedianTPMF)
+		dataFiles.GeneMedianTPM = geneMedianTPMF
+	}
+
+	// Check for geneReads
+	geneReadsF := filepath.Join(config.DataDIR, geneReads)
+	if _, err := os.Stat(annotationsF); os.IsNotExist(err) {
+		panic(fmt.Sprintf("Error does not exist:", geneReadsF))
+	} else {
+		log.Printf("geneReads file exists: %s", geneReadsF)
+		dataFiles.GeneReads = geneMedianTPMF
+	}
+
+	// Check for sampleAnnot
+	sampleAnnotF := filepath.Join(config.DataDIR, sampleAnnot)
+	if _, err := os.Stat(sampleAnnotF); os.IsNotExist(err) {
+		panic(fmt.Sprintf("Error does not exist:", sampleAnnotF))
+	} else {
+		log.Printf("sampleAnnot file exists: %s", sampleAnnotF)
+		dataFiles.SampleAnnot = sampleAnnotF
+	}
+
+	return dataFiles
+}
+
+// CreateDirIfNotExist creates the dir if it does not already exist
 func CreateDirIfNotExist(dir string) {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		err = os.MkdirAll(dir, 0755)
